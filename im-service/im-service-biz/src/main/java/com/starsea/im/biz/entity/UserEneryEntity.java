329fcc3f6ff690e2a2e42f06c1bc9aeac26bb15e
wisher12341 <601011162@qq.com>
2016-08-24 18:36:15 +0800

    UserEnery interface

---
+++ im-service/im-service-biz/src/main/java/com/starsea/im/biz/entity/UserEneryEntity.java
@@ -0,0 +1,34 @@
+package com.starsea.im.biz.entity;
+
+/**
+ * Created by Administrator on 2016/8/24.
+ */
+public class UserEneryEntity {
+    private Integer id;
+    private String openId;
+    private String enerySelect;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getEnerySelect() {
+        return enerySelect;
+    }
+
+    public void setEnerySelect(String enerySelect) {
+        this.enerySelect = enerySelect;
+    }
+}
---
+++ im-service/im-service-web/src/main/java/com/starsea/im/web/controller/UserEneryController.java
@@ -0,0 +1,50 @@
+package com.starsea.im.web.controller;
+
+import com.starsea.im.aggregation.service.UserEneryService;
+import com.starsea.im.aggregation.util.ServiceResult;
+import com.starsea.im.biz.entity.UserEneryEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Administrator on 2016/8/24.
+ */
+@Controller
+@RequestMapping("/userenery")
+public class UserEneryController extends AjaxBase {
+    @Autowired
+    private UserEneryService userEneryService;
+
+    @RequestMapping(value = "/addUserEnery", method = RequestMethod.POST)
+    @ResponseBody
+    public ServiceResult addEnery(HttpServletRequest request,
+                                  @RequestParam("openId") String openId,
+                                  @RequestParam("enerySelect") String enerySelect) {
+        ServiceResult serviceResult = new ServiceResult();
+        UserEneryEntity userEneryEntity = new UserEneryEntity();
+        userEneryEntity.setOpenId(openId);
+        userEneryEntity.setEnerySelect(enerySelect);
+        serviceResult.setMsg(userEneryService.addUserEnery(userEneryEntity));
+        serviceResult.setCode(200);
+        return setResponseData(serviceResult);
+    }
+
+
+    @RequestMapping(value = "/queryEnergyByOpenId",method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResult queryEnergyByOpenId(HttpServletRequest req,
+                                             @RequestParam("openId") String openId){
+        ServiceResult serviceResult = new ServiceResult();
+        serviceResult.setMsg(userEneryService.queryEneryByOpenId(openId));
+        serviceResult.setCode(200);
+        return setResponseData(serviceResult);
+    }
+
+
+}
