eefe0ec501fcfff9a10db8a562c310b09fb0ad53
wisher12341 <601011162@qq.com>
2016-09-05 22:28:25 +0800

    zidingyi

---
+++ im-service/im-service-web/src/main/webapp/static/js/util.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2016/9/5.
+ */
+
+function getNowFormatDate() {//获取当前时间,yyyy-MM-dd
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = year + seperator1 + month + seperator1 + strDate;
+    return currentdate;
+}
